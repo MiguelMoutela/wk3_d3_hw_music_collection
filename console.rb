3a30b459fd47fdfe62b2bcca8193cea4e0baeef3
Miguel <miguelmoutela@hotmail.com>
2017-10-11 17:19:32 +0100

    typos found

--- console.rb
+++ console.rb
@@ -10,9 +10,21 @@ artist_1 = Artist.new({'name' => 'artist'})
 artist_2 = Artist.new({'name' => 'another_artist'})
 artist_3 = Artist.new({'name' => 'yet_another_artist'})
 
-album_1 = Album.new({'title' => 'that_album'})
-album_2 = Album.new({'title' => 'the_other_album'})
-album_3 = Album.new({'title' => 'that_other_album'})
+album_1 = Album.new({
+  'title' => 'that_album'})
+  'year' => "2017"
+  'artist_id' => artist_1.id
+
+album_2 = Album.new({
+  'title' => 'that_album'})
+  'year' => "2017"
+  'artist_id' => artist_2.id
+
+album_3 = Album.new({
+  'title' => 'that_album'})
+  'year' => "2017"
+  'artist_id' => artist_3.id
+
 
 binding.pry
 nil
